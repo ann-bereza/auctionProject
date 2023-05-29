d6c9039f519168749d58ea5e8b1820ab1d362978
ann-bereza <annhavelia@gmail.com>
2023-05-29 19:22:30 +0300

    fix for static

--- auctionProject/settings.py
+++ auctionProject/settings.py
@@ -114,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR/ 'auctions' / 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR/ 'auctions' / 'media')
 
 
 # Default primary key field type
